ff7f1073cd1de181ddc73c00f38353a02f122d0c
James <jrh682@gmail.com>
2016-07-15 15:55:49 -0700

    troubleshooting homeModule. using commit to push

--- Modules/HomeModules.cs
+++ Modules/HomeModules.cs
@@ -21,8 +21,28 @@ namespace HairSalon
       Get["stylist/{id}"] = parameters => {
         Dictionary<string, object> Model = new Dictionary<string, object>();
         var foundStylist = Stylist.Find(parameters.id);
+        var foundClient = foundStylist.GetClient();
         Model.Add("stylist", foundStylist);
+        Model.Add("client", foundClient);
         return View["index.cshtml", Model];
+      };
+      Get["/client/new"] = _ => {
+        List<Client> allClients = Client.GetAll();
+        return View["stylist.cshtml", allClients];
+      };
+      Post["/client/new"] = _ => {
+        Client newClient = new Client(Request.Form["client_name"]);
+        newClient.Save();
+        List<Client> allClients = Client.GetAll();
+        return View["stylist.cshtml", allClients];
+      };
+      Get["/client/{id}"] = parameters => {
+        Dictionary<string, object> Model = new Dictionary<string, object>();
+        var foundClient = Client.Find(parameters.id);
+        var foundStylist = Client.Find(foundStylist.GetClient());
+        Model.Add("client", foundClient);
+        Model.Add("stylist", foundStylist);
+        return View["stylist.cshtml", Model];
       };
     }
   }
