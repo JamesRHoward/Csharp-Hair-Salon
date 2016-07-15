bb73ec82ea28b41decec0cd0e3a66fc9fec204ce
James <jrh682@gmail.com>
2016-07-15 13:13:26 -0700

    created clienttest and tested that the data base was empty and created getrs and setters and GetAll for client.cs. will test GetAll method

---
+++ Objects/Client.cs
@@ -0,0 +1,60 @@
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System;
+
+namespace HairSalon
+{
+  public class Client
+  {
+    private int _id;
+    private string _clientName;
+
+    public Client(string clientName, int id = 0)
+    {
+      _id = id;
+      _clientName = clientName;
+    }
+    public int GetId()
+    {
+      return _id;
+    }
+    public string GetClientName()
+    {
+      return _clientName;
+    }
+    public void SetClientName(string newClientName)
+    {
+      _clientName = newClientName;
+    }
+    public static List<Client> GetAll()
+    {
+      List<Client> allClients = new List<Client>{};
+
+      SqlConnection conn = DB.Connection();
+      SqlDataReader rdr = null;
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("SELECT * FROM clients;", conn);
+      rdr = cmd.ExecuteReader();
+
+      while(rdr.Read())
+      {
+        int clientId = rdr.GetInt32(0);
+        string clientName = rdr.GetString(1);
+        Client newClient = new Client(clientName, clientId);
+        allClients.Add(newClient);
+      }
+
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      if (conn != null)
+      {
+        conn.Close();
+      }
+
+      return allClients;
+    }
+  }
+}
