43ed989668ec36302d98d26d90b9accc6221a9b2
James <jrh682@gmail.com>
2016-07-15 11:48:57 -0700

    updated homemodule to have dictionary and index.cshtml to show model. test server

--- Modules/HomeModules.cs
+++ Modules/HomeModules.cs
@@ -18,6 +18,12 @@ namespace HairSalon
         List<Stylist> allStylists = Stylist.GetAll();
         return View["index.cshtml", allStylists];
       };
+      Get["stylist/{id}"] = parameters => {
+        Dictionary<string, object> Model = new Dictionary<string, object>();
+        var foundStylist = Stylist.Find(parameters.id);
+        Model.Add("stylist", foundStylist);
+        return View["index.cshtml", Model];
+      };
     }
   }
 }
