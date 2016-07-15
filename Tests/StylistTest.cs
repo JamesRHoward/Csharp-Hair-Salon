6d9d9aea2f34589fa97e08a0bfea0fad6439c161
James <jrh682@gmail.com>
2016-07-15 09:36:34 -0700

    Testing for boolean override is passing. It will have Save method built in Stylist.cs

--- Tests/StylistTest.cs
+++ Tests/StylistTest.cs
@@ -12,12 +12,22 @@ namespace HairSalon
     {
       DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=hair_salon_test;Integrated Security=SSPI;";
     }
+
     [Fact]
     public void Test_DatabaseIsEmpty()
     {
       int result = Stylist.GetAll().Count;
 
       Assert.Equal(0, result);
+    }
+
+    [Fact]
+    public void Test_ReturnsTrueIfStylistIsTheSame()
+    {
+      Stylist firstStylist = new Stylist("Sammy");
+      Stylist secondStylist = new Stylist("Sammy");
+
+      Assert.Equal(firstStylist, secondStylist);
     }
 
     public void Dispose()
