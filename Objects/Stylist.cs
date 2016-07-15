7cc59c474cf22e83fb44e5adece1adc66e51c9af
James <jrh682@gmail.com>
2016-07-15 11:08:09 -0700

    added delete method to Stylist.cs. it will have test for  delete method.

--- Objects/Stylist.cs
+++ Objects/Stylist.cs
@@ -164,6 +164,25 @@ namespace HairSalon
         rdr.Close();
       }
     }
+    public void Delete()
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("DELETE FROM stylists WHERE id = @stylistId;", conn);
+
+      SqlParameter stylistIdParameter = new SqlParameter();
+      stylistIdParameter.ParameterName = "@stylistId";
+      stylistIdParameter.Value = this.GetId();
+
+      cmd.Parameters.Add(stylistIdParameter);
+      cmd.ExecuteNonQuery();
+
+      if (conn != null);
+      {
+        conn.Close();
+      }
+    }
     public static void DeleteAll()
     {
       SqlConnection conn = DB.Connection();
