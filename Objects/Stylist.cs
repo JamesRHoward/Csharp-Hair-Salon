34e7a36f2e37d1ba4444b28ea4f8294926ded94b
James <jrh682@gmail.com>
2016-07-15 09:10:08 -0700

    Added GetAll method to stylist.cs. Build test and run dnx test.

--- Objects/Stylist.cs
+++ Objects/Stylist.cs
@@ -3,3 +3,58 @@ using System.Data.SqlClient;
 using System;
 
 namespace HairSalon
+{
+  public class Stylist
+  {
+    private int _id;
+    private string _stylist;
+
+    public Stylist(string stylist, int id = 0)
+    {
+      _id = id;
+      _stylist = stylist;
+    }
+    public int GetId()
+    {
+      return _id;
+    }
+    public string GetStylist()
+    {
+      return _stylist;
+    }
+    public void SetStylist(string newStylist)
+    {
+      _stylist = newStylist;
+    }
+    public static List<Stylist> GetAll()
+    {
+      List<Stylist> allStylists = new List<Stylist>{};
+
+      SqlConnection conn = DB.Connection();
+      SqlDataReader rdr = null;
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("SELECT * FROM stylists;", conn);
+      rdr = cmd.ExecuteReader();
+
+      while(rdr.Read())
+      {
+        int stylistId = rdr.GetInt32(0);
+        string stylistStylist = rdr.GetString(1);
+        Stylist newStylist = new Stylist(stylistStylist, stylistId);
+        allStylists.Add(newStylist);
+      }
+
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      if (conn != null)
+      {
+        conn.Close();
+      }
+
+      return allStylists;
+    }
+  }
+}
