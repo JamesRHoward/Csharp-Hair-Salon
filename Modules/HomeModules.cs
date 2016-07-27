f3759e2542db440bca8c9abcfb81a9a74f448284
James Howard <james@>
2016-07-26 20:54:36 -0700

    too late to keep working going home to sleep will finish in the morning.

--- Modules/HomeModules.cs
+++ Modules/HomeModules.cs
@@ -21,15 +21,25 @@ namespace HairSalon
       };
       Get["stylist/{id}"] = parameters => {
         Dictionary<string, object> Model = new Dictionary<string, object>();
-        var foundStylist = Stylist.Find(parameters.id);
-        var foundClient = foundStylist.GetClient();
-        Model.Add("stylist", foundStylist);
+        Stylist SelectedStylist = Stylist.Find(parameters.id);
+        List<Client> foundStylist = SelectedStylist.GetClient();
+        List<Client> foundClient = Client.GetAll();
+        Model.Add("stylist", SelectedStylist);
+        Model.Add("stylistClients", foundStylist);
         Model.Add("client", foundClient);
-        return View["index.cshtml", Model];
+        return View["stylist.cshtml", Model];
       };
       Get["/client/new"] = _ => {
         List<Client> allClients = Client.GetAll();
         return View["stylist.cshtml", allClients];
+      };
+      Get["client/{id}"] = parameters => {
+        Dictionary<string, object> Model = new Dictionary<string, object>();
+        var foundClient = Client.Find(parameters.id);
+        var foundStylist = foundClient.GetId();
+        Model.Add("client", foundClient);
+        Model.Add("stylist", foundStylist);
+        return View["stylist.cshtml", Model];
       };
       Post["/client/new"] = _ => {
         Client newClient = new Client(Request.Form["client_name"]);
@@ -37,14 +47,6 @@ namespace HairSalon
         List<Client> allClients = Client.GetAll();
         return View["stylist.cshtml", allClients];
       };
-      // Get["client/{id}"] = parameters => {
-      //   Dictionary<string, object> Model = new Dictionary<string, object>();
-      //   var foundClient = Client.Find(parameters.id);
-      //   var foundStylist = Stylist.Find(foundStylist.GetId());
-      //   Model.Add("client", foundClient);
-      //   Model.Add("stylist", foundStylist);
-      //   return View["stylist.cshtml", Model];
-      // };
     }
   }
 }
